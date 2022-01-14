f4edbfed5dfffef4ad592e76b4235e9624487ffd
Flavio <fac@pop-os.localdomain>
2022-01-14 19:29:07 -0300

    crud

--- src/index.js
+++ src/index.js
@@ -2,16 +2,39 @@ const express = require('express')
 
 const server = express()
 
-server.get('/', (req, res) => {
-    res.send('ok na home')
-})
+// server.get('/', (req, res) => {
+//     res.send('ok na home')
+// })
 
 server.get('/health', (req, res) => {
-    res.send('ok na health')
+    res
+    .status(200)
+    .json({status: "tudo certo na rota health"})
 })
 
-server.get('/hello', (req, res) => {
-    res.send('ok na hello')
+// server.get('/hello', (req, res) => {
+//     res.send('ok na hello')
+// })
+
+server.get('/itens', (req, res) => {
+    res
+    .status(200)
+    .json({status: "ok para get de itens"})
+})
+
+server.post('/itens', (req, res) => {
+    res
+    .status(201)
+    .json({ id: 001,
+            nome: "item01",
+            tipo: "utilidades",
+            valor: 100 })
+})
+
+server.delete('/itens/001', (req, res) => {
+    res
+    .status(200)
+    .json({status: "apagando o item"})
 })
 
 server.listen(3000, () => {
